a54707842a3a5ab6a427901d02a4f2d2bef57cf0
Dmitry-Titkov <dmitry.titkov94@gmail.com>
2020-11-04 08:57:05 +0100

    initial redux

--- src/pages/InputPage.js
+++ src/pages/InputPage.js
@@ -1,27 +1,40 @@
 import React from "react";
+import { useState } from "react";
+
 
 const InputPage = () => {
-  return (
+
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [household, setSelectedHousehold] = useState("");
+  const [monthlyIncome, setSelectedMonthlyIncome] = useState("");
+  const [rent, setSelectedRent] = useState("");
+  const [selectedStocks, setSelectedStocks] = useState("");
+
+    return (
     <div>
   <h1>Please fill in the form</h1>
 <br></br>
 <h2>What country are you from?</h2>
-<select id = "selColor">
-   <option value = "1">Belgium</option>
-   <option value = "2">Netherlands</option>
-   <option value = "3">Russia</option>
-   <option value = "4">Germany</option>
-   <option value = "5">France</option>
-   <option value = "6">US</option>
+  
+        <select
+          onChange={(event) => setSelectedLocation(event.target.value)}
+          className="select"
+        >
+   <option value = "Belgium">Belgium</option>
+   <option value = "Netherlands">Netherlands</option>
+   <option value = "Russia">Russia</option>
+   <option value = "Germany">Germany</option>
+   <option value = "France">France</option>
+   <option value = "US">US</option>
   </select>
   <h2>How many adults are in your household?</h2>
-  <input></input>
+  <input type= "number" onChange={(event) => setSelectedHousehold(event.target.value)}></input>
   <h2>What is your monthly income?</h2>
-  <input></input>
+  <input type= "number" onChange={(event) => setSelectedMonthlyIncome(event.target.value)}></input>
   <h2>If you own a house how much would you be able to rent it for</h2>
-  <input></input>
+  <input type= "number" onChange={(event) => setSelectedRent(event.target.value)}></input>
   <h2>If you own stocks, how much would their value be?</h2>
-  <input></input>
+  <input type= "number"onChange={(event) => setSelectedStocks(event.target.value)}></input>
 <br></br>
 <br></br>
   <button>Submit</button>
---
+++ src/store/form/actions.js
@@ -0,0 +1,6 @@
+export const POST_LOCATION = 'POST_LOCATION';
+
+export const postlocation = (location) => ({
+  type: POST_LOCATION,
+  payload: location
+})
